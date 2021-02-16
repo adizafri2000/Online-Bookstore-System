44314427be0232e9f643ea3c1c48b2c111705f67
iffahnadhrah <70426220+iffahnadhrah@users.noreply.github.com>
2021-02-16 19:14:00 +0800

    Add files via upload

---
+++ adminReg.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Chunchunmaru Bookstore</title>
+
+      <link href="reset.css" rel="stylesheet" />
+      <link href="form.css" type="text/css" rel="stylesheet" />
+    </head>
+	<body>
+	<?php include "admin-header.inc.php"; ?>
+	<div class="dform">
+		<form>
+			<h2>ADMIN REGISTRATION</h2>
+			<label>Email</label>
+			<input type="text" name="email" placeholder="Enter email">
+			<label>Username</label>
+			<input type="text" name="uname" placeholder="Enter username">
+			<label>Email</label>
+			<input type="password" name="pwd" placeholder="Enter password">
+			<button type="submit" class="btn">Register</button>
+			<p>Already an admin? <a href="#">Login</a></p>
+		</form>
+		</div>
+	</body>
+</html>
