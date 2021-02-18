1d1c2553d6608bad962ac14f64264581907d0b3c
Adi Zafri <adizafri2000@gmail.com>
2021-02-18 17:31:45 +0800

    Updated Bookview.php
    Created a base for how a book and its details will be displayed once clicked on. Add to cart part incomplete

--- bookview.php
+++ bookview.php
@@ -21,7 +21,35 @@
                     $ISBN = $_GET['ISBN'];
             ?>
             <div id="book-section">
-                
+                <div id="book-info">
+                    <div id="book-cover">
+                        <?php
+                            $connect = mysqli_connect("localhost","root","","chunchunmaru");
+                            $query = "select * from book where ISBN=".$ISBN;
+                            $result = mysqli_query($connect,$query);
+                            $row = mysqli_fetch_array($result);
+                            $link = "images/book-cover/".$row['Genre']."/".$ISBN.".jpg";
+                        ?>
+                        <img src="<?php echo $link ?>" width="210" height="270">
+                    </div>
+                    <div id="book-details">
+                        <h4 id="book-name"><?php echo $row['bookName']; ?></h4>
+                        <h5>by <?php echo $row['authorName']; ?></h5>
+                        <br>
+                        <p>Published by: <?php echo $row['pubName']; ?></p>
+                        <p>Year published: <?php echo $row['Year_Published']; ?></p>
+                        <p>ISBN: <?php echo $row['ISBN']; ?></p>
+                        <p>Price: RM<?php echo $row['Price']; ?></p>
+                    </div>
+                </div>
+                <div id="summary">
+                    <h4>Summary</h4>
+                    <p><?php echo $row['Summary']; ?></p>
+                </div>
+                <div id="adding-cart">
+                    <p><?php echo $row['Stock']?> unit(s) in stock.</p>
+                    
+                </div>
             </div>
         </div>
         <footer><?php include "footer.inc.php" ?></footer>
