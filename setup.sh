53187a2fc8ebec883ba3e80d0637eb07ff6dbeca
Adi Zafri <adizafri2000@gmail.com>
2023-02-08 01:12:28 +0800

    update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@
 
 url=""
 echo "Searching for files that contain localhost and converting them to ${url}"
-grep -Ri 'localhost' | xargs sed -i 's/localhost/${url}/g'
+grep -Ril 'localhost' . | xargs sed -i "s/localhost/${url}/g"
 
+grep -Ril 'localhost' .
+php -v
 php DDL.php
 php DML-init.php
